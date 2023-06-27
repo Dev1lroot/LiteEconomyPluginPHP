efab3c7339259ca7384705e94642604377ed375d
David Eichendorf <dev1lroot@protonmail.com>
2023-06-27 04:36:33 +0400

    Update LiteEconomy.php

--- LiteEconomy.php
+++ LiteEconomy.php
@@ -19,12 +19,12 @@ class LiteEconomy{
     }
     private function updateBalance(MinecraftPlayer $player,$value)
     {
-        // Updating database record of the player wealth
+        // Updating database record of the player wealth, it will return true|false on success|failure of the operation
         return $this->connection->query("UPDATE `{$this->table}` SET `money` = {$value} WHERE `uuid` = '{$player->uuid}' LIMIT 1");
     }
     private function createBalance(MinecraftPlayer $player,$value)
     {
-        // Inserting new record of the player wealth
+        // Inserting new record of the player wealth, it will return true|false on success|failure of the operation
         return $this->connection->query("INSERT INTO `{$this->table}` (`uuid`,`money`) VALUES ('{$player->uuid}','{$value}')");
     }
     public function getBalance(MinecraftPlayer $player)
@@ -32,7 +32,7 @@ class LiteEconomy{
         // Getting all the data about existing account
         $result = $this->selectBalance($player);
 
-        // Returning only the numeric value, or null is doesn't exists
+        // Returning only the numeric value, or null if doesn't exists
         return (isset($result->money)) ? floatval($result->money) : 0;
     }
     public function setBalance(MinecraftPlayer $player, $value)
